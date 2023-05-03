f3e3b23eb13936e63615b249bb77bace53a2cba5
Nahom <zerihunnahom@gmail.com>
2023-05-03 15:38:21 +0300

    Add normal queries related to aggregate

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
+/*Normal Queries*/
 SELECT
     *
 FROM
     animals
 WHERE
     name LIKE '%mon';
-
 
 SELECT
     name
@@ -14,7 +14,6 @@ WHERE
     date_of_birth BETWEEN '2016-01-01'
     AND '2019-12-30';
 
-
 SELECT
     name
 FROM
@@ -23,14 +22,12 @@ WHERE
     neutered = TRUE
     AND escape_attempts < 3;
 
-
 SELECT
     date_of_birth
 FROM
     animals
 WHERE
     name IN ('Agumon', 'Pikachu');
-
 
 SELECT
     name,
@@ -40,7 +37,6 @@ FROM
 WHERE
     weight_kg > 10.5;
 
-
 SELECT
     *
 FROM
@@ -48,14 +44,12 @@ FROM
 WHERE
     neutered = TRUE;
 
-
 SELECT
     *
 FROM
     animals
 WHERE
     name != 'Gabumon';
-
 
 SELECT
     *
@@ -65,7 +59,7 @@ WHERE
     weight_kg >= 10.4
     AND weight_kg <= 17.3;
 
-
+/*Transaction Queries*/
 BEGIN;
 
 UPDATE
@@ -74,7 +68,6 @@ SET
     species = 'unspecified';
 
 ROLLBACK;
-
 
 BEGIN;
 
@@ -94,14 +87,12 @@ WHERE
 
 COMMIT;
 
-
 BEGIN;
 
 DELETE FROM
     animals;
 
 ROLLBACK;
-
 
 BEGIN;
 
@@ -127,3 +118,49 @@ WHERE
     weight_kg < 0;
 
 COMMIT;
+
+/*Normal Queries*/
+SELECT
+    COUNT(*) AS animals_count
+FROM
+    animals;
+
+SELECT
+    COUNT(*) AS animals_count
+FROM
+    animals
+WHERE
+    escape_attempts = 0;
+
+SELECT
+    AVG(weight_kg) AS average_weight
+FROM
+    animals;
+
+SELECT
+    neutered,
+    SUM(escape_attempts) AS total_escape
+FROM
+    animals
+GROUP BY
+    neutered;
+
+SELECT
+    species,
+    MIN(weight_kg) AS MIN_weight,
+    MAX(weight_kg) AS MAX_weight
+FROM
+    animals
+GROUP BY
+    species;
+
+SELECT
+    species,
+    AVG(escape_attempts) AS average_attempt
+FROM
+    animals
+WHERE
+    date_of_birth BETWEEN '1990-01-01'
+    AND '2000-12-30'
+GROUP BY
+    species;
