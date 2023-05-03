4dd4a2cf1521e06d6e211903172ea9535acafd2d
Nahom <zerihunnahom@gmail.com>
2023-05-03 14:28:40 +0300

    Add transaction queries

--- queries.sql
+++ queries.sql
@@ -1,8 +1,129 @@
-SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-30';
-SELECT name FROM animals WHERE neutered=TRUE AND escape_attempts<3;
-SELECT date_of_birth FROM animals WHERE name IN ('Agumon','Pikachu');
-SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
-SELECT * FROM animals WHERE neutered=TRUE;
-SELECT * FROM animals WHERE name!='Gabumon';
-SELECT * FROM animals WHERE weight_kg>=10.4 AND weight_kg<=17.3;
+SELECT
+    *
+FROM
+    animals
+WHERE
+    name LIKE '%mon';
+
+
+SELECT
+    name
+FROM
+    animals
+WHERE
+    date_of_birth BETWEEN '2016-01-01'
+    AND '2019-12-30';
+
+
+SELECT
+    name
+FROM
+    animals
+WHERE
+    neutered = TRUE
+    AND escape_attempts < 3;
+
+
+SELECT
+    date_of_birth
+FROM
+    animals
+WHERE
+    name IN ('Agumon', 'Pikachu');
+
+
+SELECT
+    name,
+    escape_attempts
+FROM
+    animals
+WHERE
+    weight_kg > 10.5;
+
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    neutered = TRUE;
+
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    name != 'Gabumon';
+
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    weight_kg >= 10.4
+    AND weight_kg <= 17.3;
+
+
+BEGIN;
+
+UPDATE
+    animals
+SET
+    species = 'unspecified';
+
+ROLLBACK;
+
+
+BEGIN;
+
+UPDATE
+    animals
+SET
+    species = 'digimon'
+WHERE
+    name LIKE '%mon';
+
+UPDATE
+    animals
+SET
+    species = 'pokemon'
+WHERE
+    species IS NULL;
+
+COMMIT;
+
+
+BEGIN;
+
+DELETE FROM
+    animals;
+
+ROLLBACK;
+
+
+BEGIN;
+
+DELETE FROM
+    animals
+WHERE
+    date_of_birth > '2022-01-01';
+
+SAVEPOINT sp1;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * (-1);
+
+ROLLBACK TO sp1;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * (-1)
+WHERE
+    weight_kg < 0;
+
+COMMIT;
