8ba913ee3dcad893d531cbbc30afb09163e68e2c
Nahom <zerihunnahom@gmail.com>
2023-05-05 13:42:59 +0300

    Insert animals visit info into visits table

--- schema.sql
+++ schema.sql
@@ -67,5 +67,5 @@ CREATE TABLE visits(
     animals_id INT REFERENCES animals(id) ON DELETE CASCADE,
     vets_id INT REFERENCES vets(id) ON DELETE CASCADE,
     visit_date DATE NOT NULL,
-    PRIMARY KEY(animals_id, vets_id)
+    PRIMARY KEY(animals_id, vets_id,visit_date)
 );
