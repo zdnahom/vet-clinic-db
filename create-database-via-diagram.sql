76e159ae46d3297ace627d834b3a31b8b31b4354
Abigail Boateng <109290913+abenaTenkorang@users.noreply.github.com>
2023-05-09 10:35:32 +0000

    Add invoices and invoices_item table

--- create-database-via-diagram.sql
+++ create-database-via-diagram.sql
@@ -28,5 +28,34 @@ CREATE TABLE medical_histories_treatments(
     treatment_id INT NOT NULL,
     FOREIGN KEY(history_id) REFERENCES medical_histories(id) ON DELETE CASCADE,
     FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
-    PRIMARY KEY(history_id, treatment_id)
+    PRIMARY KEY(history_id, treatment_id),
+    INDEX(history_id),
+    INDEX(treatment_id)
 );
+
+CREATE TABLE invoices (
+id INT NOT NULL,
+total_amount decimal NOT NULL,
+generated_at timestamp NOT NULL,
+payed_at timestamp NOT NULL,
+medical_history_id INT NOT NULL,
+FOREIGN KEY(medical_history) REFERENCES medical_histories(id) UNIQUE ON DELETE CASCADE,
+PRIMARY KEY(id),
+INDEX (medical_history_id)
+);
+
+CREATE TABLE invoice_items (
+id INT NOT NULL,
+unit_price decimal NOT NULL,
+quantity INT NOT NULL,
+total_price decimal NOT NULL,
+invoice_id INT NOT NULL,
+treatment_id INT NOT NULL,
+FOREIGN KEY(invoice_id) REFERENCES invoices(id) ON DELETE CASCADE,
+FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
+PRIMARY KEY(id),
+INDEX(invoice_id),
+INDEX (treatment_id)
+
+)
+
