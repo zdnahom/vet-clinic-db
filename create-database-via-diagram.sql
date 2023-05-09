c5cfc6b70fcdb27ccd3767c774bbdeb1ca830f7b
Nahom <zerihunnahom@gmail.com>
2023-05-09 16:21:19 +0300

    Modify Create table

--- create-database-via-diagram.sql
+++ create-database-via-diagram.sql
@@ -13,7 +13,7 @@ CREATE TABLE medical_histories(
     patient_id INT NOT NULL,
     status VARCHAR(255),
     FOREIGN KEY(patient_id) REFERENCES patients(id) ON DELETE CASCADE,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
 );
 
 CREATE TABLE treatments(
@@ -29,33 +29,26 @@ CREATE TABLE medical_histories_treatments(
     FOREIGN KEY(history_id) REFERENCES medical_histories(id) ON DELETE CASCADE,
     FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
     PRIMARY KEY(history_id, treatment_id),
-    INDEX(history_id),
-    INDEX(treatment_id)
 );
 
 CREATE TABLE invoices (
-id INT NOT NULL,
-total_amount decimal NOT NULL,
-generated_at timestamp NOT NULL,
-payed_at timestamp NOT NULL,
-medical_history_id INT NOT NULL,
-FOREIGN KEY(medical_history) REFERENCES medical_histories(id) UNIQUE ON DELETE CASCADE,
-PRIMARY KEY(id),
-INDEX (medical_history_id)
+    id INT NOT NULL,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP NOT NULL,
+    medical_history_id INT NOT NULL,
+    FOREIGN KEY(medical_history) REFERENCES medical_histories(id) UNIQUE ON DELETE CASCADE,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE invoice_items (
-id INT NOT NULL,
-unit_price decimal NOT NULL,
-quantity INT NOT NULL,
-total_price decimal NOT NULL,
-invoice_id INT NOT NULL,
-treatment_id INT NOT NULL,
-FOREIGN KEY(invoice_id) REFERENCES invoices(id) ON DELETE CASCADE,
-FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
-PRIMARY KEY(id),
-INDEX(invoice_id),
-INDEX (treatment_id)
-
+    id INT NOT NULL,
+    unit_price DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL NOT NULL,
+    invoice_id INT NOT NULL,
+    treatment_id INT NOT NULL,
+    FOREIGN KEY(invoice_id) REFERENCES invoices(id) ON DELETE CASCADE,
+    FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
+    PRIMARY KEY(id),
 )
-
